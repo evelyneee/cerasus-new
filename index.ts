06ba719aa8787f06f730315be56a6d73ea53a9ed
ja1dan <ajaidan0@gmail.com>
2022-05-14 12:05:55 -0400

    chore: unify code practices

--- index.ts
+++ index.ts
@@ -6,56 +6,54 @@ fastify.register(require('fastify-cors'), {
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 })
 
-import * as fs from 'fs';
-import * as dotenv from "dotenv";
-
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec)
-
-import { FastifyContext, FastifyError, FastifyReply, FastifyRequest } from "fastify";
 import { randomUUID } from 'crypto';
+import * as dotenv from "dotenv";
+import { FastifyContext, FastifyError, FastifyReply, FastifyRequest } from "fastify";
+import {readFileSync, unlinkSync, writeFileSync} from 'fs';
+import { promisify } from 'util';
+
+// start dotenv
 dotenv.config();
 
+// global variables
+const exec = promisify(require('child_process').exec)
 const server = fastify
-
 const version = 1
 
-server.get('/v' + version + '/playing', async (request: FastifyRequest, reply: FastifyReply) => {
-    return await fetchTracks()
-})
+server.get(`/v${version}/playing`, async (request: FastifyRequest, reply: FastifyReply) => await fetchTracks)
 
-server.post('/v' + version + "/lottie", async (request: FastifyRequest, reply: FastifyReply) => {
-  const uuid = randomUUID()
-  fs.writeFileSync("./" + request.headers.filename + uuid + ".json", JSON.stringify(request.body))
-  await exec('./lottieconverter ' + "./" + request.headers.filename + uuid + ".json" + " -type gif 160x160 45 > " + "./" + request.headers.filename + uuid + ".gif")
-  const fsData = fs.readFileSync("./" + request.headers.filename + uuid + ".gif")
-  fs.unlinkSync("./" + request.headers.filename + uuid + ".json")
-  fs.unlinkSync("./" + request.headers.filename + uuid + ".gif")
-  return fsData
+server.post(`/v${version}/lottie`, async (request: FastifyRequest, reply: FastifyReply) => {
+    const uuid = randomUUID()
+    writeFileSync(`./${request.headers.filename}${uuid}.json`, JSON.stringify(request.body))
+    await exec(`./lottieconverter ./${request.headers.filename}${uuid}.json -type gif 160x160 45 > + ./${request.headers.filename}${uuid}.gif`)
+    const fsData = readFileSync(`./${request.headers.filename}${uuid}.gif`)
+    unlinkSync(`./${request.headers.filename}${uuid}.json`)
+    unlinkSync(`./${request.headers.filename}${uuid}.gif`)
+    return fsData
 })
 
 server.get('/v1-alpha.1' + '/links', async (request: FastifyRequest, reply: FastifyReply) => {
     console.log(request.url)
-    const res = await fetch('https://api.song.link' + request.url)
+    const res = await fetch(`https://api.song.link${request.url}`)
     return res.json()
 })
 
-server.listen(8080, "::", (err: FastifyError, address: FastifyContext) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
+server.listen(8080, '::', (err: FastifyError, address: FastifyContext) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listening at ${address}`)
 })
 
-async function fetchTracks() {
-  const response = await fetch('https://api.music.apple.com/v1/me/recent/played/tracks', {
-    method: 'GET',
-    headers: {
-        'Authorization': process.env.DEV_TOKEN!,
-        'Music-User-Token': process.env.USER_TOKEN!,
-        'Content-Type': 'application/json'
-    }
-  })
-  return response.json()
+let fetchTracks = async () => {
+    const response = await fetch('https://api.music.apple.com/v1/me/recent/played/tracks', {
+        method: 'GET',
+        headers: {
+            'Authorization': process.env.DEV_TOKEN!,
+            'Music-User-Token': process.env.USER_TOKEN!,
+            'Content-Type': 'application/json'
+        }
+    })
+    return response.json()
 }
